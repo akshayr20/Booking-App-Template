ac897859ced4dd6c13bda95bcbf5234471b2c154
akshayr20 <akshay.rajput5394@gmail.com>
2019-01-02 10:30:41 +0530

    added sass media query manager, to make app responsive, WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { AboutComponent } from './about/about.component';
-import { FeaturesComponent } from './features/features.component';
-import { ToursComponent } from './tours/tours.component';
-import { StoriesComponent } from './stories/stories.component';
-import { BookingComponent } from './booking/booking.component';
-import { FooterComponent } from './footer/footer.component';
+// import { HeaderComponent } from './header/header.component';
+// import { NavigationComponent } from './navigation/navigation.component';
+// import { AboutComponent } from './about/about.component';
+// import { FeaturesComponent } from './features/features.component';
+// import { ToursComponent } from './tours/tours.component';
+// import { StoriesComponent } from './stories/stories.component';
+// import { BookingComponent } from './booking/booking.component';
+// import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +14,14 @@ import { FooterComponent } from './footer/footer.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  components = [
-    NavigationComponent,
-    HeaderComponent,
-    AboutComponent,
-    FeaturesComponent,
-    ToursComponent,
-    StoriesComponent,
-    BookingComponent,
-    FooterComponent
-  ];
+  // components = [
+  //   NavigationComponent,
+  //   HeaderComponent,
+  //   AboutComponent,
+  //   FeaturesComponent,
+  //   ToursComponent,
+  //   StoriesComponent,
+  //   BookingComponent,
+  //   FooterComponent
+  // ];
 }
