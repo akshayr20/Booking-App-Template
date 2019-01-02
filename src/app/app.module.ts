9b30ee2e9d20d479648f1e0ebc3ce30def4bc054
akshayr20 <akshay.rajput5394@gmail.com>
2019-01-02 10:49:40 +0530

    removed unnecessary packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ScrollingModule } from '@angular/cdk/scrolling';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { COMMON_COMPONENTS } from './commonComponents';
 
 @NgModule({
   declarations: [AppComponent, COMMON_COMPONENTS],
-  entryComponents: [COMMON_COMPONENTS],
-  imports: [BrowserModule, ScrollingModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
