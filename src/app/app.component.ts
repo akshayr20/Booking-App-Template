b8b32516aae13b19d3981c876fc297df083d4f37
akshayr20 <akshay.rajput5394@gmail.com>
2019-01-03 12:53:40 +0530

    removed commented code, and unrequired package(express), to host application on github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,8 @@
 import { Component } from '@angular/core';
-// import { HeaderComponent } from './header/header.component';
-// import { NavigationComponent } from './navigation/navigation.component';
-// import { AboutComponent } from './about/about.component';
-// import { FeaturesComponent } from './features/features.component';
-// import { ToursComponent } from './tours/tours.component';
-// import { StoriesComponent } from './stories/stories.component';
-// import { BookingComponent } from './booking/booking.component';
-// import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  // components = [
-  //   NavigationComponent,
-  //   HeaderComponent,
-  //   AboutComponent,
-  //   FeaturesComponent,
-  //   ToursComponent,
-  //   StoriesComponent,
-  //   BookingComponent,
-  //   FooterComponent
-  // ];
-}
+export class AppComponent {}
